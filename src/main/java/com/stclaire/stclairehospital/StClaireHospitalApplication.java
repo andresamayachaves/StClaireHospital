0fc7a485894cef0ff28c370b1544af8c1acd4465
andresamayachaves <andresamayachaves@gmail.com>
2022-07-27 20:41:06 -0500

    First Commit

---
+++ src/main/java/com/stclaire/stclairehospital/StClaireHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.stclaire.stclairehospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StClaireHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StClaireHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stclaire/stclairehospital/StClaireHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stclaire.stclairehospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StClaireHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
